16260569e02a90e62489222143b9b082639aa837
HoangGiang2004 <giangcute402@gmail.com>
2023-07-02 17:32:48 +0700

    final test

---
+++ bai2.js
@@ -0,0 +1,8 @@
+
+    const a = parseInt(prompt("Nhập chiều cao của  tam giác:"));
+    for(let i = 1; i<=a;i++){
+        for(let j = 1;j<= i; j++){
+          document.write("*");
+        }
+        document.write("<br />");
+    }
